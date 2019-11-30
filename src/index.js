8778bb1ba4318162a108ad01f945b5ff1f00b03c
Robert Górnicki <github@gorki.net.pl>
2019-11-30 14:27:29 +0100

    add image, gsap examples and return link to home page

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import './index.scss';
 
 import * as toolkit from './shared/js/toolkit';
 
+import gsap from 'gsap';
+
 document.addEventListener('DOMContentLoaded', () => {
   toolkit.sayHello('index.js');
 });
+
+const logos = [].slice.call(document.querySelectorAll('.logo'));
+
+gsap.to(logos, {duration: 1, rotation: '360deg', repeat: -1, repeatDelay: 1, stagger: 0.5, ease: 'power1.inOut'});
